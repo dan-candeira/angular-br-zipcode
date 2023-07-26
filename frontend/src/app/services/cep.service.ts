c89b08e25dd18335c39487d8ea1aba1fdb697e65
dan-candeira <danielbarr16@gmail.com>
2023-07-26 16:33:24 -0300

    feat: add navbar component + cep service requests

--- frontend/src/app/services/cep.service.ts
+++ frontend/src/app/services/cep.service.ts
@@ -1,11 +1,48 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import {
+  Observable,
+  Subject,
+  catchError,
+  combineLatest,
+  map,
+  of,
+  switchMap,
+  tap,
+} from 'rxjs';
+import { Cep } from '../models/cep';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CepService {
+  public findLocations$ = new Subject<Array<string>>();
+  public getLocations$: Observable<Array<Cep>> = this.findLocations$.pipe(
+    switchMap((ceps) =>
+      combineLatest(
+        ceps.map((cep) =>
+          this.httpClient.get<Cep>(`https://viacep.com.br/ws/${cep}/json/`)
+        )
+      ).pipe(
+        map((resonse): Array<Cep> => {
+          const filtered = resonse
+            .map((r) => {
+              if ((r as any)['erro']) {
+                // toastr to display erros
+                return null;
+              }
+              return r;
+            })
+            .filter((r) => r !== null);
 
-  constructor(private httpClient: HttpClient) { }
+          return filtered as Array<Cep>;
+        }),
+        catchError((error) => {
+          return of([]);
+        })
+      )
+    )
+  );
 
+  constructor(private httpClient: HttpClient) {}
 }
